154fffe2824ef726b0bfa3f2534eb5804d5a90d5
qsrdev <matteo.albertini747@gmail.com>
2025-04-11 13:36:46 +0200

    feat: 02 complete

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,16 +8,16 @@
 // Stampa tutti i valori nella console.
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
-// e le variabili sopra in modo da stampare correttamente 
+// e le variabili sopra in modo da stampare correttamente
 // in console la stringa con il nome completo
-const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
-const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
-const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // NON MODIFICARE QUESTA RIGA
+const firstName = "Jane"; // NON MODIFICARE QUESTA RIGA
+const lastName = "Smith"; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = `${firstName} ${lastName}`;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
